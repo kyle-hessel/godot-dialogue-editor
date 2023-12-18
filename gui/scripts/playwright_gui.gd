2b3d836ad8dbdf55c4a8269c3e59af51c817fd26
PollutedMind <pollutedmind@proton.me>
2023-12-17 19:29:57 -0700

    Set PlaywrightGUI to display PlaywrightGraph, a GraphEdit node. Container Sizing has to have Vertical set to Expand otherwise nothing shows up.

--- gui/scripts/playwright_gui.gd
+++ gui/scripts/playwright_gui.gd
@@ -1,5 +1,9 @@
 @tool
 extends Control
 
+# This Control needs Vertical Container Sizing set to Expand. See: https://github.com/godotengine/godot/issues/34497
+
+@onready var playwright_graph: GraphEdit = $PlaywrightGraph
+
 func _enter_tree():
-	print("hello")
+	pass
