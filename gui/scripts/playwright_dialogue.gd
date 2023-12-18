cfb32b0b319f868270a450bf7bc17bf39142ac43
PollutedMind <pollutedmind@proton.me>
2023-12-17 22:47:45 -0700

    Enabled ports for each dialogue line and added a button for adding dialogues.

--- gui/scripts/playwright_dialogue.gd
+++ gui/scripts/playwright_dialogue.gd
@@ -7,10 +7,15 @@ extends GraphNode
 @onready var add_line_button: Button = $DialogueVBox/AddLineButton
 @onready var dialogue_text_edit: TextEdit = $DialogueTextEdit
 
+var dialogue_type: Dialogue.DialogueType = Dialogue.DialogueType.DEFAULT
+var slot_index: int = 1
+
 func _ready():
-	pass
+	set_slot_color_left(slot_index, Color.hex(0x4781d1))
+	set_slot_color_right(slot_index, Color.hex(0xb74243))
 
 func _on_dialogue_type_option_button_item_selected(index: int):
+	dialogue_type = index
 	match index:
 		0:
 			add_line_button.text = "Add Line"
@@ -23,3 +28,9 @@ func _on_add_line_button_pressed():
 	var new_dialogue_text: TextEdit = dialogue_text_edit.duplicate()
 	new_dialogue_text.text = ""
 	add_child(new_dialogue_text)
+	
+	slot_index += 1
+	#call_deferred("set_slot", true, 0, Color.BLUE, true, 0, Color.RED)
+	set_slot(slot_index, true, 0, Color.hex(0x4781d1), true, 0, Color.hex(0xb74243))
+
+
--- gui/scripts/playwright_gui.gd
+++ gui/scripts/playwright_gui.gd
@@ -1,9 +1,14 @@
 @tool
+# This Control needs Vertical Container Sizing set to Expand. See: https://github.com/godotengine/godot/issues/34497
 extends Control
 
-# This Control needs Vertical Container Sizing set to Expand. See: https://github.com/godotengine/godot/issues/34497
+const PlaywrightDialogue: PackedScene = preload("res://addons/playwright/gui/scenes/playwright_dialogue.tscn")
 
 @onready var playwright_graph: GraphEdit = $PlaywrightGraph
+@onready var add_dialogue_button: Button = $AddDialogueButton
 
 func _enter_tree():
 	pass
+
+func _on_add_dialogue_button_pressed():
+	playwright_graph.add_child(PlaywrightDialogue.instantiate())
