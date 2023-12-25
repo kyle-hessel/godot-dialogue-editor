49473a971624619e61a9254fc4d7eece51de8d42
PollutedMind <pollutedmind@proton.me>
2023-12-25 00:20:50 -0700

    Added dialogue node connections and a serialize button, which for now just prints the playwright_graph's connection list (an array of dictionaries).

--- gui/scripts/playwright_gui.gd
+++ gui/scripts/playwright_gui.gd
@@ -12,3 +12,13 @@ func _enter_tree():
 
 func _on_add_dialogue_button_pressed():
 	playwright_graph.add_child(PlaywrightDialogue.instantiate())
+
+func _on_serialize_dialogue_button_pressed():
+	var dialogue_connection_list: Array[Dictionary] = playwright_graph.get_connection_list()
+	print(dialogue_connection_list)
+
+func _on_playwright_graph_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int):
+	playwright_graph.connect_node(from_node, from_port, to_node, to_port)
+
+func _on_playwright_graph_disconnection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int):
+	playwright_graph.disconnect_node(from_node, from_port, to_node, to_port)
